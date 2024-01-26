c253fe52e0ceb5df5d194539cc55417086bb3a80
teru12012000 <sion2769@gmail.com>
2024-01-26 21:58:50 +0900

    fix: add url

--- src/shared/data/api/apiUrl.ts
+++ src/shared/data/api/apiUrl.ts
@@ -1,7 +1,7 @@
 export const apiUrl: string[] = [
-    "http://picsum.photos/v2/list?page=1&limit=10",
-    "http://picsum.photos/v2/list?page=2&limit=10",
-    "http://picsum.photos/v2/list?page=3&limit=10",
-    "http://picsum.photos/v2/list?page=4&limit=10",
-    "http://picsum.photos/v2/list?page=5&limit=10",
+    "https://picsum.photos/v2/list?page=1&limit=10",
+    "https://picsum.photos/v2/list?page=2&limit=10",
+    "https://picsum.photos/v2/list?page=3&limit=10",
+    "https://picsum.photos/v2/list?page=4&limit=10",
+    "https://picsum.photos/v2/list?page=5&limit=10",
 ]
