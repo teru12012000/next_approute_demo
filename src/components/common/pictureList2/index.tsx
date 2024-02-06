89e2d278756a1a2425fe60a63d31526d4953aa06
teru12012000 <sion2769@gmail.com>
2024-02-06 22:39:38 +0900

    refactor: add view.tsx

--- src/components/common/pictureList2/index.tsx
+++ src/components/common/pictureList2/index.tsx
@@ -1,29 +1,14 @@
 import { pictureType } from "@/shared/types/apiType"
 import styles from "./style.css"
 import Image from "next/image"
+import PictureList2View from "./view"
 
 interface props {
     data?: pictureType[]
 }
 
 const PicutreList2 = (props: props) => {
-    return (
-        <div className={styles.containar}>
-            {props.data?.map((item: pictureType, index: number) => (
-                <div key={index} className={styles.itemMargin}>
-                    <Image
-                        src={item.download_url}
-                        width={320}
-                        height={240}
-                        alt={item.id}
-                    />
-                    <p>
-                        {item.id}:{item.author}
-                    </p>
-                </div>
-            ))}
-        </div>
-    )
+    return <PictureList2View {...props} />
 }
 
 export default PicutreList2
