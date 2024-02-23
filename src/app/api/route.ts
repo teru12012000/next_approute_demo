a55764ef6a387af2ce5ad7b1c93fa547e5f87c8d
teru12012000 <sion2769@gmail.com>
2024-02-23 17:19:21 +0900

    fix: delete console

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -1,7 +1,5 @@
 export function GET(request: Request) {
     const random: number = Math.floor(Math.random() * 20) + 1
 
-    console.log(random)
-
     return Response.json({ id: String(random) })
 }
